5cabcca939258b4cfdaa9dac6ba02e263e723c5d
Ricardo L. Dani <ricardodani@gmail.com>
2012-12-03 20:51:51 -0200

    Completed field instrospection in any model.

--- queryset_reporter/core.py
+++ queryset_reporter/core.py
@@ -1,9 +1,65 @@
 # -*- encoding: utf-8 -*-
 
-class Queryset(object):
-    def __init__(self, queryset_report):
-        pass
+from django.db.models.fields.related import RelatedField
+from django.db.models.related import RelatedObject
 
-class Report(object):
-    def __init__(self):
-        pass
+
+def _direct_field(field, only_normal=False):
+    field_metadata = {
+        'name': field.get_attname(),
+        'verbose': field.verbose_name,
+        'type': field.__class__.__name__,
+    }
+    # verify if it`s a RelatedField
+    if isinstance(field, RelatedField) and only_normal:
+        return None
+    elif isinstance(field, RelatedField):
+        # means that the field is OneToOneField, ManyToMany or a ForeignKey
+        field_metadata.update({
+            'lookup_fields': get_model_fields(field.rel.to, only_normal=True),
+        })
+    return field_metadata
+
+
+def _related_object(field):
+    # for a non direct fields
+    if isinstance(field, RelatedObject):
+        return {
+            'name': field.var_name,
+            'verbose': field.model._meta.verbose_name_plural,
+            'type': field.__class__.__name__,
+            'lookup_fields': get_model_fields(
+                field.field.model, only_normal=True),
+        }
+    else:
+        print 'isto nao deveria acontecer'
+        return None
+
+
+def field_meta(field, only_normal=False):
+    # First verify if `field` is direct or not
+    if field[2]:
+        # If the field is direct, means that it can be a ForeigKey,
+        # ManyToManyField or a <?>Field (? = Char, Integer, ...)
+        return _direct_field(field[0], only_normal)
+    elif not only_normal:
+        return _related_object(field[0])
+    else:
+        None
+
+
+def get_model_fields(model, only_normal=False):
+    # get_field_by_name(field_name)
+    # Returns the (field_object, model, direct, m2m), where field_object is
+    # the Field instance for the given name, model is the model containing
+    # this field (None for local fields), direct is True if the field exists
+    # on this model, and m2m is True for many-to-many relations. When
+    # 'direct' is False, 'field_object' is the corresponding RelatedObject
+    # for this field (since the field doesn't have an instance associated
+    # with it).
+    fields = []
+    for field_name in model._meta.get_all_field_names():
+        f = field_meta(model._meta.get_field_by_name(field_name), only_normal)
+        if f:
+            fields.append(f)
+    return fields
--- queryset_reporter/static/queryset_reporter/queryset_reporter.js
+++ queryset_reporter/static/queryset_reporter/queryset_reporter.js
@@ -8,12 +8,16 @@
  * */
 
 (function($) {
-    $(document).ready(function() {
-        $('#queryset_form #id_model').change(function(event) {
-            var model = $(this);
-            if (model.val().length > 0) {
-                $.getJSON('/queryset_reporter/ajax/model-fields/')
-            }
-        }).change();
-    })
+// django jquery namespace
+$(document).ready(function() {
+    $('#queryset_form #id_model').change(function(event) {
+        var model = $(this);
+        if (model.val().length > 0) {
+            var url = '/queryset_reporter/ajax/model-fields/';
+            $.getJSON(url, {model: model.val()}, function(data) {
+                console.log(data);
+            });
+        }
+    }).change();
+})
 }(django.jQuery));
