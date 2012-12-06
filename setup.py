dbbb065f243322060e69a60276928d337fb58f6c
Ricardo L. Dani <ricardodani@gmail.com>
2012-12-06 11:42:19 -0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='django-queryset-reporter',
+    version='0.1',
+    description='A django admin-site app to persist queryset`s and then make file reports with them.',
+    author='Ricardo Dani',
+    author_email='ricardod@horizonte.tv.br',
+    url='https://github.com/ricardodani/django-queryset-reporter.git',
+    packages=['queryset_reporter'],
+)
