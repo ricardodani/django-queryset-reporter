cd9e398c48223c50faabc68b5aa6682bd471f79c
Allan Figueiredo <allanf@globosat.com.br>
2012-12-12 12:58:05 -0200

    Corrigindo problema de referencia para pacotes

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     install_requires=[
         'django>= 1.3',
         'south',
-        'grappelli>= 2.3.8',
+        'django-grappelli== 2.3.9',
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
